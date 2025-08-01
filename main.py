7b488f02deee264219aee087c7ede1b18440f2a3
Aina <novativeai@gmail.com>
2025-08-01 15:00:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,11 @@ load_dotenv()
 app = FastAPI()
 
 # --- CORS Middleware ---
-origins = ["http://localhost:3000"]
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8081",
+    "https://ai-video-generator-mvp.netlify.app"
+]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
